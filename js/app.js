9ec9680b082ce1bae7f7a4a9f38a26a8fb11d9bf
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-03-01 18:24:26 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let instancesLine = 0;
+let stationInstances = 0;
 import { Station, Network, Line, drawLine, drawStation} from './drawItems.js';
 
   let net = new Network([]);
@@ -6,10 +7,10 @@ import { Station, Network, Line, drawLine, drawStation} from './drawItems.js';
   let mosX;
   let mosY;
   export function updateDisplay(event) {
-    mosX = Math.round((event.pageX-10)/25)*25;
-    mosY = Math.round((event.pageY-10)/25)*25;
+    mosX = Math.round((event.pageX-15)/40)*40;
+    mosY = Math.round((event.pageY-15)/40)*40;
   };
-  let default_fNames = ['fontenay', 'vincennes', 'nation', 'auber', 'gare de lyon', 'cergy']
+  let default_fNames = ['fontenay', 'VINCENNES', 'nation', 'auber', 'gare de lyon', 'cergy']
   let default_sNames = ['sous-bois', 'le-haut', 'centre', 'porte', 'le-pecq', 'préfécture']
   function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -19,20 +20,14 @@ import { Station, Network, Line, drawLine, drawStation} from './drawItems.js';
 canvas.addEventListener('mouseenter', updateDisplay, false);
 canvas.addEventListener("mousemove", updateDisplay, false);
 canvas.addEventListener('click', function(){
-  const context = canvas.getContext('2d');
-    if(net.lines[instancesLine] == undefined){
-      net.lines[instancesLine] = new Line(instancesLine, 'ligne_' + instancesLine, 3, '#FF000F', [new Station(default_fNames[getRandomIntInclusive(0, 5)], default_sNames[getRandomIntInclusive(0, 5)], 'destination', 'rect', mosX, mosY)])
-      drawStation(net.lines[instancesLine].stations[net.lines[instancesLine].stationInstances].fName + '\n' + net.lines[instancesLine].stations[net.lines[instancesLine].stationInstances].sName, net.lines[instancesLine].stations[net.lines[instancesLine].stationInstances].style, net.lines[instancesLine].stations[net.lines[instancesLine].stationInstances].type, mosX, mosY, net.lines[instancesLine].color);
-      net.lines[instancesLine].stationInstances++;
-    }else{
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      net.lines[instancesLine].stations[net.lines[instancesLine].stationInstances] = new Station(default_fNames[getRandomIntInclusive(0, 5)], default_sNames[getRandomIntInclusive(0, 5)], 'common', 'circle', mosX, mosY);
-      for(const station of net.lines[instancesLine].stations){
-        drawLine(net.lines[instancesLine].color, net.lines[instancesLine].lineThicness, net.lines[instancesLine].stations[net.lines[instancesLine].stationInstances-1].xPos, net.lines[instancesLine].stations[net.lines[instancesLine].stationInstances-1].yPos, net.lines[instancesLine].stations[net.lines[instancesLine].stationInstances].xPos, net.lines[instancesLine].stations[net.lines[instancesLine].stationInstances].yPos);
-        drawStation(station.fName + ' ' + station.sName, station.style, station.type, station.xPos, station.yPos, net.lines[instancesLine].color);
-      }
-      net.lines[instancesLine].stationInstances++;
-
-  }
-  
+  if(stationInstances == 0 && net.lines[instancesLine] == undefined){
+    net.lines[instancesLine] = new Line(instancesLine, 'ligne_' + instancesLine, "5", prompt('entrer la couleur de la ligne'), [new Station(default_fNames[getRandomIntInclusive(0, 5)], default_sNames[getRandomIntInclusive(0, 5)], 'destination', 'rect', mosX, mosY)]);
+    drawStation(net.lines[instancesLine].stations[0].fName, net.lines[instancesLine].stations[0].sName, net.lines[instancesLine].stations[0].style, net.lines[instancesLine].stations[0].type, mosX, mosY, net.lines[instancesLine].color);
+    stationInstances++;
+  }else{
+    net.lines[instancesLine].stations[stationInstances] = new Station(default_fNames[getRandomIntInclusive(0, 5)], default_sNames[getRandomIntInclusive(0, 5)], 'common', 'circle', mosX, mosY);
+    drawStation(net.lines[instancesLine].stations[stationInstances].fName, net.lines[instancesLine].stations[stationInstances].sName, net.lines[instancesLine].stations[stationInstances].style, net.lines[instancesLine].stations[stationInstances].type, mosX, mosY, net.lines[instancesLine].color);
+    drawLine(net.lines[instancesLine].color, net.lines[instancesLine].lineThicness, net.lines[instancesLine].stations[stationInstances-1].xPos, net.lines[instancesLine].stations[stationInstances-1].yPos, net.lines[instancesLine].stations[stationInstances].xPos, net.lines[instancesLine].stations[stationInstances].yPos); 
+    stationInstances++;
+    }
 }, false);
--- js/drawItems.js
+++ js/drawItems.js
@@ -16,69 +16,183 @@ export function Line(id, name, lineThicness, color, stations){
     this.lineThicness = lineThicness;
     this.color = color;
     this.stations = stations;
-    this.stationInstances = 0;
     this.selected = false;
 };
-export function drawStation(name, style, type, posX, posY, lineColor) {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    ctx.shadowBlur = 15;
-    ctx.textAlign = 'center';
-    ctx.fontWeight = '500';
-    ctx.font = '16px verdana';
-    if(type == 'destination' || type == 'exchange'){
-        ctx.strokeStyle = "black";
-        ctx.fillStyle = "white";
-        if(style == 'rect'){
-            ctx.lineWidth = 2;
-            ctx.strokeRect(posX, posY, 15, 15);
-            ctx.fillRect(posX, posY, 15, 15);
-            ctx.fillStyle = "black";
-            ctx.fillText(name, posX+7.5, posY-10);
-        }else if(style == 'inter'){
-            ctx.lineWidth = 1;
-            ctx.beginPath();
-            ctx.roundRect(posX+3.75, posY-7.5, 15, 30, 8);
-            ctx.fill();
-            ctx.fillStyle = "black";
-            ctx.fillText(name, posX+3.75, posY-10);
-        }
-        else if(style == 'circle'){
-            ctx.lineWidth = 1;
-            ctx.beginPath();
-            ctx.arc(posX+7.5, posY-7.5, 10, 0, 2 * Math.PI);
-            ctx.fill();
-            ctx.fillStyle = "black";
-            ctx.fillText(name, posX+7.5, posY-10);
-        }
-        ctx.stroke(); 
-    }else{
-        ctx.strokeStyle = lineColor;
-        ctx.fillStyle = lineColor;
-        if(style == 'rect'){
-            ctx.lineWidth = 2;
-            ctx.fillRect(posX, posY, 10, 10);
-            ctx.fillStyle = "black";
-            ctx.fillText(name, posX+5, posY-10);
-        }else if(style == 'circle'){
-            ctx.lineWidth = 1;
-            ctx.beginPath();
-            ctx.arc(posX+5, posY+5, 5, 0, 2 * Math.PI);
-            ctx.fill();
-            ctx.fillStyle = "black";
-            ctx.fillText(name, posX+5, posY-10);
-        }
-        ctx.stroke(); 
+let instances = 0;
+export function drawStation(fName,sName, style, type, posX, posY, lineColor) {
+    var svgns = "http://www.w3.org/2000/svg";
+    switch(type){
+        case 'destination':
+            if(style == 'rect' && type != 'exchange'){
+                var rect = document.createElementNS( svgns, style);
+                rect.setAttributeNS( null,'id',instances);
+                rect.setAttributeNS( null,'x',posX-15);
+                rect.setAttributeNS( null,'y',posY-15);
+                rect.setAttributeNS( null,'width', '30');
+                rect.setAttributeNS( null,'height','30');
+                rect.setAttributeNS( null,'fill', '#FFFFFF');
+                rect.setAttributeNS( null,'stroke-width', '3');
+                rect.setAttributeNS( null,'stroke', '#000000');
+                document.getElementById( 'canvas' ).appendChild( rect );
+
+                var first_name = document.createElementNS(svgns, 'text');
+                var second_name = document.createElementNS(svgns, 'text');
+                first_name.innerHTML = fName+'-'+sName;
+                first_name.setAttributeNS(null, 'x', posX);
+                first_name.setAttributeNS(null, 'y', posY-30);
+                first_name.setAttributeNS(null, 'class', 'stationNames');
+                first_name.setAttributeNS(null, 'font-size', '16');
+                first_name.setAttributeNS(null, 'font-family', 'verdana');
+                first_name.setAttributeNS(null, 'font-weight', '700');
+                document.getElementById( 'canvas' ).appendChild( first_name );
+                // second_name.innerHTML = sName;
+                // second_name.setAttributeNS(null, 'x', posX-rect.getAttributeNS(null ,'width')*1.35);
+                // second_name.setAttributeNS(null, 'y', posY-25);
+                // second_name.setAttributeNS(null, 'class', 'stationNames');
+                // second_name.setAttributeNS(null, 'font-size', '16');
+                // second_name.setAttributeNS(null, 'font-family', 'verdana');
+                // second_name.setAttributeNS(null, 'font-weight', '700');
+                // document.getElementById( 'canvas' ).appendChild( second_name );
+            }else if(style == 'circle'){
+                var circle = document.createElementNS( svgns, style);
+                circle.setAttributeNS( null,'id',instances);
+                circle.setAttributeNS( null,'cx',posX-15);
+                circle.setAttributeNS( null,'cy', posY-15);
+                circle.setAttributeNS( null,'r', '20');
+                circle.setAttributeNS( null,'fill','#FFFFFF');
+                circle.setAttributeNS( null,'stroke-width', '3');
+                circle.setAttributeNS( null,'stroke', '#000000');
+                document.getElementById( 'canvas' ).appendChild( circle );
+                
+                var first_name = document.createElementNS(svgns, 'text');
+                var second_name = document.createElementNS(svgns, 'text');
+                first_name.innerHTML = fName+'-'+sName;
+                first_name.setAttributeNS(null, 'x', posX);
+                first_name.setAttributeNS(null, 'y', posY-30);
+                first_name.setAttributeNS(null, 'class', 'stationNames');
+                first_name.setAttributeNS(null, 'font-size', '16');
+                first_name.setAttributeNS(null, 'font-family', 'verdana');
+                first_name.setAttributeNS(null, 'font-weight', '700');
+                document.getElementById( 'canvas' ).appendChild( first_name );
+                // second_name.innerHTML = sName;
+                // second_name.setAttributeNS(null, 'x', posX-rect.getAttributeNS(null ,'width')*1.35);
+                // second_name.setAttributeNS(null, 'y', posY-25);
+                // second_name.setAttributeNS(null, 'class', 'stationNames');
+                // second_name.setAttributeNS(null, 'font-size', '16');
+                // second_name.setAttributeNS(null, 'font-family', 'verdana');
+                // second_name.setAttributeNS(null, 'font-weight', '700');
+                // document.getElementById( 'canvas' ).appendChild( second_name );
+            }
+            instances++;
+            break;
+        case 'exchange':
+            var inter = document.createElementNS( svgns, style);
+            inter.setAttributeNS( null,'id',instances);
+            inter.setAttributeNS( null,'id',instances);
+            inter.setAttributeNS( null,'x',posX);
+            inter.setAttributeNS( null,'y',posY+15);
+            inter.setAttributeNS( null,'width', '40');
+            inter.setAttributeNS( null,'height','80');
+            inter.setAttributeNS( null,'rx', '20');
+            inter.setAttributeNS( null,'ry','20');
+            inter.setAttributeNS( null,'fill','#FFFFFF');
+            inter.setAttributeNS( null,'stroke-width', '3');
+            inter.setAttributeNS( null,'stroke', '#000000');
+            document.getElementById( 'canvas' ).appendChild( inter );
+
+            var first_name = document.createElementNS(svgns, 'text');
+            var second_name = document.createElementNS(svgns, 'text');
+            first_name.innerHTML = fName+'-'+sName;
+            first_name.setAttributeNS(null, 'x', posX);
+            first_name.setAttributeNS(null, 'y', posY-30);
+            first_name.setAttributeNS(null, 'class', 'stationNames');
+            first_name.setAttributeNS(null, 'font-size', '16');
+            first_name.setAttributeNS(null, 'font-family', 'verdana');
+            first_name.setAttributeNS(null, 'font-weight', '700');
+            document.getElementById( 'canvas' ).appendChild( first_name );
+            // second_name.innerHTML = sName;
+            // second_name.setAttributeNS(null, 'x', posX-inter.getAttributeNS(null ,'width'));
+            // second_name.setAttributeNS(null, 'y', posY-25);
+            // second_name.setAttributeNS(null, 'class', 'stationNames');
+            // second_name.setAttributeNS(null, 'font-size', '16');
+            // second_name.setAttributeNS(null, 'font-family', 'verdana');
+            // second_name.setAttributeNS(null, 'font-weight', '700');
+            // document.getElementById( 'canvas' ).appendChild( second_name );
+            instances++;
+            break;
+        case 'common':
+            if(style == 'rect' && type != 'exchange'){
+                var rect = document.createElementNS( svgns, style);
+                rect.setAttributeNS( null,'id',instances);
+                rect.setAttributeNS( null,'x',posX);
+                rect.setAttributeNS( null,'y',posY);
+                rect.setAttributeNS( null,'width', '20');
+                rect.setAttributeNS( null,'height','20');
+                rect.setAttributeNS( null,'fill',lineColor);
+                document.getElementById( 'canvas' ).appendChild( rect );
+
+                var first_name = document.createElementNS(svgns, 'text');
+                var second_name = document.createElementNS(svgns, 'text');
+                first_name.innerHTML = fName+'-'+sName;
+                first_name.setAttributeNS(null, 'x', posX);
+                first_name.setAttributeNS(null, 'y', posY-30);
+                first_name.setAttributeNS(null, 'class', 'stationNames');
+                first_name.setAttributeNS(null, 'font-size', '16');
+                first_name.setAttributeNS(null, 'font-family', 'verdana');
+                first_name.setAttributeNS(null, 'font-weight', '700');
+                document.getElementById( 'canvas' ).appendChild( first_name );
+                // second_name.innerHTML = sName;
+                // second_name.setAttributeNS(null, 'x', posX-rect.getAttributeNS(null ,'width'));
+                // second_name.setAttributeNS(null, 'y', posY-25);
+                // second_name.setAttributeNS(null, 'class', 'stationNames');
+                // second_name.setAttributeNS(null, 'font-size', '16');
+                // second_name.setAttributeNS(null, 'font-family', 'verdana');
+                // second_name.setAttributeNS(null, 'font-weight', '700');
+                // document.getElementById( 'canvas' ).appendChild( second_name );
+            }else if(style == 'circle'){
+                var circle = document.createElementNS( svgns, style);
+                circle.setAttributeNS( null,'id',instances);
+                circle.setAttributeNS( null,'cx',posX);
+                circle.setAttributeNS( null,'cy', posY);
+                circle.setAttributeNS( null,'r', '10');
+                circle.setAttributeNS( null,'fill',lineColor);
+                document.getElementById( 'canvas' ).appendChild( circle );
+
+                var first_name = document.createElementNS(svgns, 'text');
+                var second_name = document.createElementNS(svgns, 'text');
+                first_name.innerHTML = fName+'-'+sName;
+                first_name.setAttributeNS(null, 'x', posX);
+                first_name.setAttributeNS(null, 'y', posY-30);
+                first_name.setAttributeNS(null, 'class', 'stationNames');
+                first_name.setAttributeNS(null, 'font-size', '16');
+                first_name.setAttributeNS(null, 'font-family', 'verdana');
+                first_name.setAttributeNS(null, 'font-weight', '700');
+                document.getElementById( 'canvas' ).appendChild( first_name );
+                // second_name.innerHTML = sName;
+                // second_name.setAttributeNS(null, 'x', posX-circle.getAttributeNS(null ,'width'));
+                // second_name.setAttributeNS(null, 'y', posY-25);
+                // second_name.setAttributeNS(null, 'class', 'stationNames');
+                // second_name.setAttributeNS(null, 'font-size', '16');
+                // second_name.setAttributeNS(null, 'font-family', 'verdana');
+                // second_name.setAttributeNS(null, 'font-weight', '700');
+                // document.getElementById( 'canvas' ).appendChild( second_name );
+            }
+            instances++;
+            break;
     }
 
   };
 export function drawLine(color, thicness, beginX, beginY, endX, endY){
-    const canvas = document.getElementById('st_canvas');
-    const ctx = canvas.getContext('2d');
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = thicness;
-    ctx.moveTo(beginX+5, beginY+5);
-    ctx.lineTo(endX+5, endY+5);
-    ctx.stroke();
+    var svgns = "http://www.w3.org/2000/svg";
+    var line = document.createElementNS( svgns, 'line');
+    line.setAttributeNS( null,'id',instances);
+    line.setAttributeNS( null,'id',instances);
+    line.setAttributeNS( null,'x1',beginX);
+    line.setAttributeNS( null,'y1',beginY);
+    line.setAttributeNS( null,'x2',endX);
+    line.setAttributeNS( null,'y2',endY);
+    line.setAttributeNS( null,'fill', color);
+    line.setAttributeNS( null,'stroke-width', thicness);
+    line.setAttributeNS( null,'stroke', color);
+    document.getElementById( 'canvas' ).appendChild( line );
 };
