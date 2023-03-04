ecb6feb1e80f609f01d782cb60b7db06be18b2f7
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-03-04 14:29:06 +0100

    Add files via upload

--- js/export/export.js
+++ js/export/export.js
@@ -1,26 +1,26 @@
 export function saveMap(){
-//get svg element.
-var svg = document.getElementById("canvas");
+  //get svg element.
+  var svg = document.getElementById("canvas");
 
-//get svg source.
-var serializer = new XMLSerializer();
-var source = serializer.serializeToString(svg);
+  //get svg source.
+  var serializer = new XMLSerializer();
+  var source = serializer.serializeToString(svg);
 
-//add name spaces.
-if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
+  //add name spaces.
+  if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
     source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
-}
-if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
+  }
+  if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
     source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
-}
+  }
 
-//add xml declaration
-source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+  //add xml declaration
+  source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
 
-//convert svg source to URI data scheme.
-var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
+  //convert svg source to URI data scheme.
+  var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
 
-//set url value to a element's href attribute.
-document.getElementById("exportmap").href = url;
-//you can download svg file by right click menu.
+  //set url value to a element's href attribute.
+  document.getElementById("exportsvg").href = url;
+  //you can download svg file by right click menu.
 }
