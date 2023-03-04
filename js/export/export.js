fa817f3ba2192d74ad7fd065588cc48accbc91f5
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-03-04 11:47:30 +0100

    Update export.js

--- js/export/export.js
+++ js/export/export.js
@@ -1,26 +1,11 @@
-export function saveMap(){
-  //get svg element.
-  var svg = document.getElementById("canvas");
+function downloadSVGasTextFile() {
+  const base64doc = btoa(unescape(encodeURIComponent(document.querySelector('svg').outerHTML)));
+  const a = document.createElement('a');
+  const e = new MouseEvent('click');
 
-  //get svg source.
-  var serializer = new XMLSerializer();
-  var source = serializer.serializeToString(svg);
-
-  //add name spaces.
-  if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
-    source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
-  }
-  if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
-    source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
-  }
-
-  //add xml declaration
-  source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
-
-  //convert svg source to URI data scheme.
-  var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
-
-  //set url value to a element's href attribute.
-  document.getElementById("exportsvg").href = url;
-  //you can download svg file by right click menu.
+  a.download = 'download.svg';
+  a.href = 'data:text/html;base64,' + base64doc;
+  a.dispatchEvent(e);
 }
+
+downloadSVGasTextFile();
