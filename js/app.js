92a6fe9371f1327be2c3fa73f87a5b5360bb1722
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-03-05 12:46:12 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -22,22 +22,11 @@ import { saveMap } from './export/export.js'
 canvas.addEventListener('mouseenter', updateDisplay, false);
 canvas.addEventListener("mousemove", updateDisplay, false);
 canvas.addEventListener('click', function(){
-  //as in st = station
-  const st_list = document.getElementById('st-list');
-  for(let i = 0; i < instancesLine; i++){
-    for(let j = 0; j < stationInstances; j++){
-      var station = document.createElement('button');
-      station.setAttributeNS(null, 'class', 'title_station');
-      station.setAttributeNS(null, 'id', j);
-      station.setAttributeNS(null, 'onclick', 'showOptions("st-editor")');
-      station.innerHTML = net.lines[j].stations[i].fName + ' ' + net.lines[j].stations[i].sName;
-      st_list.appendChild(station);
-    }
-  }
   if(stationInstances == 0 && net.lines[instancesLine] == undefined){
     net.lines[instancesLine] = new Line(instancesLine, 'ligne_' + instancesLine, "5", colors[getRandomIntInclusive(0, 7)], [new Station(prompt('entrer le nom de la station'), '', 'destination', prompt('indiquez le type connexion de la station: (a ou b):') , 'rect', mosX, mosY)]);
     drawStation(net.lines[instancesLine].stations[0].fName, net.lines[instancesLine].stations[0].sName, net.lines[instancesLine].stations[0].style, net.lines[instancesLine].stations[0].type, mosX, mosY, net.lines[instancesLine].color);
-    stationInstances++;
+    net.lines[instancesLine].stationInstances++;
+    stationInstances = net.lines[instancesLine].stationInstances;
   }else{
     net.lines[instancesLine].stations[stationInstances] = new Station(prompt('entrer le nom de la station'), '', 'common', prompt('indiquez le type connexion de la station: (a ou b):'), 'circle', mosX, mosY);
     const canvas = document.getElementById('canvas');
@@ -48,22 +37,41 @@ canvas.addEventListener('click', function(){
     for(const element of net.lines[instancesLine].stations){
       drawStation(element.fName, element.sName, element.style, element.type, element.xPos, element.yPos, net.lines[instancesLine].color);
     }
-    stationInstances++;
+    net.lines[instancesLine].stationInstances++;
+    stationInstances = net.lines[instancesLine].stationInstances;
     }
+    //as in st = station
+  const st_list = document.getElementById('st-list');
+  for(let i = 0; i < instancesLine; i++){
+    for(let j = 0; j < stationInstances; j++){
+      var station = document.createElement('button');
+      station.setAttributeNS(null, 'class', 'title_station');
+      station.setAttributeNS(null, 'id', j);
+      station.setAttributeNS(null, 'onclick', 'showOptions("st-editor")');
+      station.setAttributeNS(null, 'clicked', 'false');
+      station.innerHTML = net.lines[j].stations[i].fName + ' ' + net.lines[j].stations[i].sName;
+      st_list.appendChild(station);
+    }
+  }
 }, false);
-
 let selectedStation = 0;
-const station = document.getElementsByClassName('title_station');
-station.addEventListener('click', function(){
-  selectedStation = station.getAttribute("id");
-});
+for(let i = 0; i < stationInstances; i++){
+  const stations = document.getElementsByClassName('title_station');
+  stations[i].addEventListener('click', function(){
+    stations[i].setAttributeNS(null, 'clicked', true);
+  }, false);
+  if(stations[i].getAttributeNS(null, 'clicked') == true){
+    selectedStation = i;
+  }
+}
+
 
 //csa = Connection style A
 csb = document.getElementById('csb');
 csa = document.getElementById('csa');
 csa.addEventListener('click', function(){
-  net.lines[0].stations[selectedStation].line_style = 'a';
-})
+  net.lines[instancesLine].stations[selectedStation].line_style = 'a';
+}, false);
 csb.addEventListener('click', function(){
-  net.lines[0].stations[selectedStation].line_style = 'a';
-})
+  net.lines[instancesLine].stations[selectedStation].line_style = 'b';
+}, false);
