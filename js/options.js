f5d1caca3adec1ff99a6d3a9a4c620ee43af9bc2
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-03-05 13:49:03 +0100

    Add files via upload

--- js/options.js
+++ js/options.js
@@ -1,3 +1,5 @@
+import { instancesLine } from "./app";
+import { Network, net} from "./drawItems";
 function showOptions(classname){
     document.getElementById(classname).style.display = 'block';
 }
@@ -7,4 +9,15 @@ function hideOptions(classname){
 function showEditor(hidden, showed){
     document.getElementById(hidden).style.display = 'none';
     document.getElementById(showed).style.display = 'block';
+}
+
+function stationsList(network){
+    var st_list = document.getElementById('st-list');
+    var stationButton = document.createElement('button');
+    for(station of network.lines[instancesLine].stations){
+        stationButton.innerText = station.fName;
+        stationButton.setAttributeNS(null, 'id', network.lines[instancesLine].stations.indexOf(station));
+        stationButton.setAttributeNS(null, 'class', 'station_instance');
+    }
+    st_list.appendChild(stationButton);
 }
