bc2b549afb3c3cd7ade4a81ada91b4f4ccd1a7a5
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-03-13 16:46:54 +0100

    Update export.js

--- js/export/export.js
+++ js/export/export.js
@@ -1,12 +1,8 @@
-function saveMap(){
-  //Get SVG element
-  var svgData = $("#canvas")[0].outerHTML;
-  var svgBlob = new Blob([svgData], {type:"image/svg+xml;charset=utf-8"});
-  var svgUrl = URL.createObjectURL(svgBlob);
-  var downloadLink = document.createElement("a");
-  downloadLink.href = svgUrl;
-  downloadLink.download = "unnamedtransportmap.svg";
-  document.body.appendChild(downloadLink);
-  downloadLink.click();
-  document.body.removeChild(downloadLink);
-}
+var exportbtn = document.getElementById('exportmap');
+var canvas = document.getElementbyId('canvas');
+
+exportbtn.addEventListener("click", function(){
+
+  (event) => (event.target.href = canvas.toDataURL());
+
+}, false);
