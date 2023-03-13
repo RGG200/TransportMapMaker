8251b817c9295d3051706a519076db47f8fc001e
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-03-13 11:59:35 +0100

    Update export.js

--- js/export/export.js
+++ js/export/export.js
@@ -1,27 +1,11 @@
 function saveMap(){
-  //get svg element.
-  var svg = document.getElementById("canvas");
-
-  //get svg source.
-  var serializer = new XMLSerializer();
-  var source = serializer.serializeToString(svg);
-
-  //add name spaces.
-  if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
-    source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
-  }
-  if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
-    source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
-  }
-
-  //add xml declaration
-  source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
-
-  //convert svg source to URI data scheme.
-  var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
-
-  //set url value to a element's href attribute.
-  document.getElementById("exportsvg").href = url;
-  //you can download svg file by right click menu.
+  //get svg element.var svgData = $("#figureSvg")[0].outerHTML;
+var svgBlob = new Blob([svgData], {type:"image/svg+xml;charset=utf-8"});
+var svgUrl = URL.createObjectURL(svgBlob);
+var downloadLink = document.createElement("a");
+downloadLink.href = svgUrl;
+downloadLink.download = "unnamedtransportmap.svg";
+document.body.appendChild(downloadLink);
+downloadLink.click();
+document.body.removeChild(downloadLink);
 }
-saveMap();
