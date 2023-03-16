737aa2c7b2514b421d7cd8bb716d60382d76b34e
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-03-16 17:54:52 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -94,9 +94,9 @@ var save = document.getElementById('save');
 function updateCanvas(){
   const canvas = document.getElementById('canvas');
   canvas.innerHTML = "";
-  for(let i = 1; i <= net.lines[instancesLine].stationInstances; i++){
+  for(let i = 1; i < net.lines[instancesLine].stationInstances; i++){
     drawLine(net.lines[instancesLine].color, net.lines[instancesLine].lineThicness, net.lines[instancesLine].stations[i-1].xPos, net.lines[instancesLine].stations[i-1].yPos, net.lines[instancesLine].stations[i].xPos, net.lines[instancesLine].stations[i].yPos, net.lines[instancesLine].stations[i].line_style, instancesLine);
-    drawStation(net.lines[instancesLine].stations[i-1].fName, net.lines[instancesLine].stations[i-1].sName, net.lines[instancesLine].stations[i-1].style, net.lines[instancesLine].stations[i-1].type, net.lines[instancesLine].stations[i-1].xPos, net.lines[instancesLine].stations[i-1].yPos, net.lines[instancesLine].color);
+    drawStation(net.lines[instancesLine].stations[i].fName, net.lines[instancesLine].stations[i].sName, net.lines[instancesLine].stations[i].style, net.lines[instancesLine].stations[i].type, net.lines[instancesLine].stations[i].xPos, net.lines[instancesLine].stations[i].yPos, net.lines[instancesLine].color);
   }
   drawStation(net.lines[instancesLine].stations[0].fName, net.lines[instancesLine].stations[0].sName, net.lines[instancesLine].stations[0].style, net.lines[instancesLine].stations[0].type, net.lines[instancesLine].stations[0].xPos, net.lines[instancesLine].stations[0].yPos, net.lines[instancesLine].color);
 }
