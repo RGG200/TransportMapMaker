06c5cdf40729466e938659719de7f3430fe16995
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-03-16 06:48:08 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -54,6 +54,7 @@ canvas.addEventListener('click', function(){
     }
     net.lines[instancesLine].stationInstances++;
     }
+    drawStationsList(net, instancesLine);
 }, false);
 
 const st_list = document.getElementById('st-list');
@@ -82,12 +83,9 @@ const buttonPressed = e => {
   document.getElementById('style-btn').innerHTML = data.style_type;
   
 }
-st_list.addEventListener("mouseenter", function (){
-  drawStationsList(net, instancesLine);
-  for (let button of buttonGroup) {
-    button.addEventListener("click", buttonPressed, false);  
-  }
-});
+for (let button of buttonGroup) {
+  button.addEventListener("click", buttonPressed, false);  
+}
 observer.observe(st_list, { attributes: true, childList: true })
 
 const save = document.getElementById('save');
