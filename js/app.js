99fb8f5dd65471a20576cdaa090b1bfa32f8037b
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-03-17 10:00:10 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
-export let instancesLine = 0;
-export let stationInstances = 0;
+export let instancesLine = 0; // counts the line instances ( gonna remove that soon I think )
 import { Station, Network, Line, drawLine, drawStation, drawStationsList, setStationText} from './drawItems.js';
 
-  let net = new Network([]);
+  let net = new Network([]); //initial network
 
-  let mosX;
+  let mosX; // mouse position on the canvas
   let mosY;
   export function updateDisplay(event) {
-    mosX = Math.round((event.pageX-10)/20)*20;
+    mosX = Math.round((event.pageX-10)/20)*20; // make it so that the stuff goes in a grid
     mosY = Math.round((event.pageY-10)/20)*20;
   };
-  let default_fNames = ['fontenay', 'VINCENNES', 'nation', 'auber', 'gare de lyon', 'cergy', 'gare du nord', 'sartrouville'];
-  let default_sNames = ['sous-bois', 'le-haut', 'centre', 'porte', 'le-pecq', 'préfécture'];
-  let colors = ['aqua', 'red', "blue", 'purple', 'orangered', 'red', 'yellow', 'green'];
+  let default_fNames = ['fontenay', 'VINCENNES', 'nation', 'auber', 'gare de lyon', 'cergy', 'gare du nord', 'sartrouville', 'poissy']; // default first station names
+  let default_sNames = ['sous-bois', 'le-haut', 'centre', 'porte', 'le-pecq', 'préfécture']; // défault complementary names
+  let colors = ['aqua', 'red', "blue", 'purple', 'orangered', 'red', 'yellow', 'green', 'crimson', 'black']; // Line colors
   function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -22,28 +21,22 @@ canvas.addEventListener('mouseenter', updateDisplay, false);
 canvas.addEventListener("mousemove", updateDisplay, false);
 canvas.addEventListener('click', function(){
   if(net.lines[instancesLine] == undefined){
-    net.lines[instancesLine] = new Line(instancesLine, 'ligne_' + instancesLine, "5", colors[getRandomIntInclusive(0, 7)], [new Station(prompt('entrer le nom de la station'), '', 'destination', prompt('indiquez le type connexion de la station: (a ou b):') , 'rect', mosX, mosY)]);
-    drawStation(net.lines[instancesLine].stations[0].fName, net.lines[instancesLine].stations[0].sName, net.lines[instancesLine].stations[0].style, net.lines[instancesLine].stations[0].type, mosX, mosY, net.lines[instancesLine].color);
+    net.lines[instancesLine] = new Line(instancesLine, 'ligne_' + instancesLine, "5", colors[getRandomIntInclusive(0, 9)], [new Station(default_fNames[getRandomIntInclusive(0, 8)], '', 'destination', 'a' , 'rect', mosX, mosY)]); // make a new line and station
+    drawStation(net.lines[instancesLine].stations[0].fName, net.lines[instancesLine].stations[0].sName, net.lines[instancesLine].stations[0].style, net.lines[instancesLine].stations[0].type, mosX, mosY, net.lines[instancesLine].color); // draw stations
+    net.lines[instancesLine].stationInstances++; // add station instances
+  }else if(net.lines[instancesLine].stationInstances == 0){
+    drawStation(net.lines[instancesLine].stations[0].fName, net.lines[instancesLine].stations[0].sName, net.lines[instancesLine].stations[0].style, net.lines[instancesLine].stations[0].type, mosX, mosY, net.lines[instancesLine].color); // draw stations
     net.lines[instancesLine].stationInstances++;
   }else{
-    let xArray = [];
-    let yArray = [];
-    let posI = 0;
-    net.lines[instancesLine].stations[net.lines[instancesLine].stationInstances] = new Station(default_fNames[getRandomIntInclusive(0, 7)], '', 'destination', 'a', 'rect', mosX, mosY);
+    net.lines[instancesLine].stations[net.lines[instancesLine].stationInstances] = new Station(default_fNames[getRandomIntInclusive(0, 8)], '', 'destination', 'a', 'rect', mosX, mosY);
     const canvas = document.getElementById('canvas');
     canvas.innerHTML = "";
     for(let i = 1; i <= net.lines[instancesLine].stationInstances; i++){
-      xArray[posI] = net.lines[instancesLine].stations[posI].xPos;
-      yArray[posI] = net.lines[instancesLine].stations[posI].yPos;
-      posI++;
       drawLine(net.lines[instancesLine].color, net.lines[instancesLine].lineThicness, net.lines[instancesLine].stations[i-1].xPos, net.lines[instancesLine].stations[i-1].yPos, net.lines[instancesLine].stations[i].xPos, net.lines[instancesLine].stations[i].yPos, net.lines[instancesLine].stations[i].line_style, instancesLine);
       if(net.lines[instancesLine].stations[i-1].connected == false && i > 1){
         net.lines[instancesLine].stations[i-1].connected = true;
       }
     }
-    xArray[posI] = net.lines[instancesLine].stations[posI].xPos;
-    yArray[posI] = net.lines[instancesLine].stations[posI].yPos;
-    posI++;
     for(const element of net.lines[instancesLine].stations){
       if(element.connected == false){ 
         element.type = "destination";
@@ -102,10 +95,10 @@ function updateCanvas(){
 }
 
 save.addEventListener('click', function(){
+  drawStationsList(net, instancesLine);
   net.lines[instancesLine].stations[id_selected_station_on_editor].fName = document.getElementById('first').value;
   net.lines[instancesLine].stations[id_selected_station_on_editor].sName = document.getElementById('second').value;
   net.lines[instancesLine].stations[id_selected_station_on_editor].style = document.getElementById('style-btn').innerHTML;
   net.lines[instancesLine].stations[id_selected_station_on_editor].line_style = document.getElementById('cx-btn').innerHTML;
-  drawStationsList(net, instancesLine);
   updateCanvas();
 });
