4c52ae307abe8ebec8add614a31dc5670dce0948
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-03-21 21:42:00 +0100

    Add files via upload

--- js/export/export.js
+++ js/export/export.js
@@ -1,4 +1,8 @@
 function exportSVG(){
-  document.getElementById('svg-canvas').style.backgroundImage = '';
-  svgExport.downloadSvg(getElementbyId('svg-canvas'), 'transport-map', {width: 2000, height: 2000);
+    document.getElementById('svg-canvas').style.background = 'none';
+    svgExport.downloadSvg(document.getElementById('svg-canvas'), 'transport-map', {width: 2000, height: 2000});
+    return true;
+}
+function resetSVG(){
+    document.getElementById('svg-canvas').style.background = 'url("../img/grid.svg")';
 }
