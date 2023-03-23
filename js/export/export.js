7cf5e59985ef0855bd532f252d5ebb8889dc160c
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-03-23 21:36:58 +0100

    Update export.js

--- js/export/export.js
+++ js/export/export.js
@@ -1,6 +1,6 @@
 function exportSVG(){
     document.getElementById('svg-canvas').style.background = 'none';
-    svgExport.downloadSvg(document.getElementById('svg-canvas'), 'transport-map', {width: 2000, height: 2000});
+    svgExport.downloadSvg(document.getElementById('svg-canvas'), 'transport-map', {width: 2000, height: 2000, scale: 0.5});
     return true;
 }
 function exportPNG(){
