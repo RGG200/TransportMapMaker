9542fa888eb96fd6983b80acfb7354f286d113e5
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-03-24 09:54:53 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -52,8 +52,8 @@ export function updateDisplay(event) {
           this.setAttributeNS(null, 'stroke', '#00FF00');
           this.setAttributeNS(null, 'stroke-width', '5');
           selected_station = stationd.id;
-          linePathId = net.lines[instancesLine].linePath.length;
           net.lines[instancesLine].linePath[linePathId] = net.lines[instancesLine].stations[selected_station];
+          linePathId = net.lines[instancesLine].linePath.length;
           is_any_station_selected = true;   
         }, true);
       }
