6b00b1ec3bb7fc412c7383f035e8089c11e2cdff
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-04-03 08:24:57 +0200

    Update export-rtm.js

--- js/FileOptions/export-rtm.js
+++ js/FileOptions/export-rtm.js
@@ -1,5 +1,5 @@
 export function exportRTM(network){
-    var file = new Blob([JSON.stringify(network, null, 2)], {type : 'application/json'});
+    var file = new Blob([JSON.stringify(network, null, 2)], {data: 'application/json'});
     const href = URL.createObjectURL(file);
     const a = Object.assign(document.createElement('a'),
     {
