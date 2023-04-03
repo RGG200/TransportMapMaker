dd36f7022d690ae953f49a103fec4bd0ec5176cd
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-04-03 11:11:26 +0200

    Update export-rtm.js

--- js/FileOptions/export-rtm.js
+++ js/FileOptions/export-rtm.js
@@ -1,12 +1,12 @@
 export function exportRTM(network){
-    var file = new Blob([JSON.stringify(network, null, 2)], {type: ''});
+    var file = new Blob([JSON.stringify(network, null, 2)], {type: 'application/json'});
     const href = URL.createObjectURL(file);
     const a = Object.assign(document.createElement('a'),
     {
         href,
         style: "display:none",
     });
-    a.download = "transportmap.rtm";
+    a.download = "transportmap";
     document.body.appendChild(a);
     a.click();
 }
