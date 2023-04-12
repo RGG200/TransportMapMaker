1000c44a8c28314810d90166c57281da66bd6d2f
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-04-12 17:37:13 +0200

    Update export.js

--- js/FileOptions/export.js
+++ js/FileOptions/export.js
@@ -8,22 +8,10 @@ function exportSVG(filename, filewidth, fileheight){
 }
 function exportPNG(filename, filewidth, fileheight){
   document.getElementById('svg-canvas').style.background = 'white';
-  const svg = document.getElementById('svg-canvas');
-  const {x, y, width, height) = svg.viewBox.baseVal;
-  const blob = new Blob([svg.outerHTML], {type: image/svg+xml'});
-  const url = URL.createObjectURL(blob);
-  const image = document.createElement('img');
-  image.src = url;
-  image.addEventListener('load', () => {
-    const canvas = document.createElement('canvas');
-    canvas.width = width;
-    canvas.height = height;
-    const context = canvas.getContext("2d");
-    context.drawImage(image, x, y, width, height);
-    const link = document.getElementById("link");
-    link.href = canvas.toDataURL();
-    URL.revokeObjectURL(url);
-  });
+    svgExport.downloadPng(document.getElementById('svg-canvas'), `${filename}`, {
+        width: filewidth,
+        height: fileheight,
+    });
   return true;
 }
 // function exportJPG(){
