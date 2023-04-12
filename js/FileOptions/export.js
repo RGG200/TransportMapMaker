b5608c0c11a8e2901dddbd29842c78d343516a3a
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-04-12 17:40:56 +0200

    Update export.js

--- js/FileOptions/export.js
+++ js/FileOptions/export.js
@@ -8,10 +8,7 @@ function exportSVG(filename, filewidth, fileheight){
 }
 function exportPNG(filename, filewidth, fileheight){
   document.getElementById('svg-canvas').style.background = 'white';
-    svgExport.downloadPng(document.getElementById('svg-canvas'), `${filename}`, {
-        width: filewidth,
-        height: fileheight,
-    });
+  svgExport.downloadPng(document.getElementById('svg-canvas'), `${filename}`, { width: filewidth, height: fileheight });
   return true;
 }
 // function exportJPG(){
