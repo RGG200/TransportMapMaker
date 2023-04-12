9a18f621e75fbdba5b98f7829ed10fd237aa0b1c
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-04-12 17:05:18 +0200

    Update export.js

--- js/FileOptions/export.js
+++ js/FileOptions/export.js
@@ -1,5 +1,5 @@
 function exportSVG(filename, filewidth, fileheight){
-    document.getElementById('svg-canvas').style.background = 'none';
+    document.getElementById('svg-canvas').style.background = 'white';
     svgExport.downloadSvg(document.getElementById('svg-canvas'), `${filename}`, {
         width: filewidth,
         height: fileheight,
@@ -7,7 +7,7 @@ function exportSVG(filename, filewidth, fileheight){
     return true;
 }
 function exportPNG(filename, filewidth, fileheight){
-    document.getElementById('svg-canvas').style.background = 'none';
+    document.getElementById('svg-canvas').style.background = 'white';
     svgExport.downloadPng("<svg id=\"svg-canvas\"></svg>", `${filename}`, {
         width: filewidth,
         height: fileheight,
@@ -16,7 +16,7 @@ function exportPNG(filename, filewidth, fileheight){
     return true;
 }
 // function exportJPG(){
-//     document.getElementById('svg-canvas').style.background = 'none';
+//     document.getElementById('svg-canvas').style.background = 'white';
 //     svgExport.downloadJpeg(document.getElementById('svg-canvas'), 'transport-map', {width: 2000, height: 2000});
 //     return true;
 // }
