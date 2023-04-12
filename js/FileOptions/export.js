98bbb4fc975c85ae5ff3452fc6781efddf197bbc
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-04-12 17:22:31 +0200

    Update export.js

--- js/FileOptions/export.js
+++ js/FileOptions/export.js
@@ -7,13 +7,24 @@ function exportSVG(filename, filewidth, fileheight){
     return true;
 }
 function exportPNG(filename, filewidth, fileheight){
-    document.getElementById('svg-canvas').style.background = 'white';
-    svgExport.downloadPng(document.querySelector("#svg-canvas"), filename, {
-        width: filewidth,
-        height: fileheight,
-        scale: 10
-    });
-    return true;
+  document.getElementById('svg-canvas').style.background = 'white';
+  const svg = document.getElementById('svg-canvas');
+  const {x, y, width, height) = svg.viewBox.baseVal;
+  const blob = new Blob([svg.outerHTML], {type: image/svg+xml'});
+  const url = URL.createObjectURL(blob);
+  const image = document.createElement('img');
+  image.src = url;
+  image.addEventListener('load', () => {
+    const canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    const context = canvas.getContext('2d");
+    context.drawImage(image, x, y, width, height);
+    const link = document.getElementById("link");
+    link.href = canvas.toDataURL();
+    URL.revokeObjectURL(url);
+  });
+  return true;
 }
 // function exportJPG(){
 //     document.getElementById('svg-canvas').style.background = 'white';
