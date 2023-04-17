0238d7addeb5afac560b360f996e76c7f679a8a8
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-04-17 18:40:35 +0200

    hopefully

--- js/FileOptions/export.js
+++ js/FileOptions/export.js
@@ -1,14 +1,14 @@
 function exportSVG(network){
     document.getElementById('svg-canvas').style.background = 'white';
-    svgExport.downloadSvg(document.getElementById('svg-canvas'), "transport-map", {
-        width: network.filewidth,
-        height: network.fileheight,
+    svgExport.downloadSvg(document.getElementById('svg-canvas'), network.filename, {
+        width: network.width,
+        height: network.height,
     });
     return true;
 }
 function  exportPNG(network){
   document.getElementById('svg-canvas').style.background = 'white';
-  svgExport.downloadPng(document.getElementById('svg-canvas'), "transport-map", { width: network.filewidth, height: network.fileheight});
+  svgExport.downloadPng(document.getElementById('svg-canvas'), "transport-map", { width: network.width, height: network.height});
   return true;
 }
 // function exportJPG(){
