56a5401227ebef15fd5bebebba30aea26fb28566
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-09-13 18:02:21 +0200

    Update export.js

--- js/FileOptions/export.js
+++ js/FileOptions/export.js
@@ -8,7 +8,7 @@ function exportSVG(name, width, height){
 }
 function exportPNG(name, width, height){
   document.getElementById('svg-canvas').style.background = '#ffffff';
-  svgExport.downloadPng(document.getElementById('svg-canvas'), `${name}`, { width: width, height: height});
+  svgExport.downloadPng(document.getElementById('svg-canvas'), `${name}`, { width: width, height: height, scale: 5});
   return true;
 }
 // function exportJPG(){
