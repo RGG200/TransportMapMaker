0924675b7b7a77f94e3447f24d6917a93e0b099f
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-09-16 13:41:59 +0200

    oops

--- js/app.js
+++ js/app.js
@@ -119,7 +119,7 @@ export function updateDisplay(event) {
   function isDrawableUnique(stationID, lineID) {
     for(const line of net.lines){
       for(const station of line.stations){
-        if(station.xPos-10 <= net.lines[lineID].stations[stationID].xPos && station.yPos-station.height/2 <= net.lines[lineID].stations[stationID].yPos && net.lines.indexOf(line) != lineID || station.xPos+10 >= net.lines[lineID].stations[stationID].xPos && station.yPos+station.height/2 >= net.lines[lineID].stations[stationID].yPos && net.lines.indexOf(line) != lineID){
+        if(station.xPos-10 <= net.lines[lineID].stations[stationID].xPos && station.yPos-station.height/2 <= net.lines[lineID].stations[stationID].yPos && net.lines.indexOf(line) != lineID && station.xPos+10 >= net.lines[lineID].stations[stationID].xPos && station.yPos+station.height/2 >= net.lines[lineID].stations[stationID].yPos && net.lines.indexOf(line) != lineID){
           return false;
         }
       }
