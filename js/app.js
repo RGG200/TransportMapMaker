ffdff753667b4029a437039d513c942eec9b6493
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-09-20 14:48:19 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -163,7 +163,6 @@ canvas.addEventListener('click', function(){
         net.lines[instancesLine].stationInstances = net.lines[instancesLine].stations.length;
         updateCanvas();
         station_is_being_created = true
-        is_any_station_selected = false
       }else{
         updateCanvas();
       }
