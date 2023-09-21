d503513d0cf51eb9e718f729b59e4df485348a25
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-09-21 09:50:11 +0200

    Update export.js x10

--- js/FileOptions/export.js
+++ js/FileOptions/export.js
@@ -8,41 +8,10 @@ function exportSVG(name, width, height){
 }
 function exportPNG(name, width, height){
   document.getElementById('svg-canvas').style.background = '#ffffff';
-  const svgNode = document.querySelector('svg-canvas');
-  const svgString = (new XMLSerializer()).serializeToString(svgNode);
-  const svgBlob = new Blob([svgString], {
-    type: 'image/svg+xml;charset=utf-8'
-  });
-
-  const DOMURL = window.URL || window.webkitURL || window;
-  const url = DOMURL.createObjectURL(svgBlob);
-  const image = new Image();
-  image.width = svgNode.width.baseVal.value;
-  image.height = svgNode.height.baseVal.value;
-  image.src = url;
-  image.onload = function () {
-    const canvas = document.getElementById('canvas');
-    canvas.width = image.width;
-    canvas.height = image.height;
-
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(image, 0, 0);
-    DOMURL.revokeObjectURL(url);
-
-    const imgURI = canvas
-      .toDataURL('image/png')
-      .replace('image/png', 'image/octet-stream');
-    triggerDownload(imgURI);
-  };
-  var href = imgURI
-  const a = Object.assign(document.createElement('a'),
-    {
-        href,
-        style: "display:none",
+      svgExport.downloadPng(document.getElementById('svg-canvas'), name, {
+        width: width*10,
+        height: height*10
     });
-  a.download = name + ".png";
-  document.body.appendChild(a);
-  a.click();
   return true;
 }
 // function exportJPG(){
