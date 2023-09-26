8ca864fd4875df77437bac68238a78fa3d62e5c1
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-09-26 09:06:35 +0200

    Update app.js35

--- js/app.js
+++ js/app.js
@@ -220,9 +220,9 @@ export function updateDisplay(event) {
   function isDrawableUnique(stationID, lineID) {
     for(const line of net.lines){
       for(const station of line.stations){
-        var overlapX = Math.abs(Math.max(0, Math.min(station.xPos + 10, net.lines[lineID].stations[stationID].xPos + 10) - Math.max(station.xPos, net.lines[lineID].stations[stationID].xPos)));
-        var overlapY = Math.abs(Math.max(0, Math.min(station.yPos + station.height, net.lines[lineID].stations[stationID].yPos + net.lines[lineID].stations[stationID].height) - Math.max(station.yPos, net.lines[lineID].stations[stationID].yPos)));
-        if(overlapX*overlapY > 0 && overlapX*overlapY <= 5 && net.lines.indexOf(line) != lineID){net.lines[lineID].stations[stationID].xPos = station.xPos;net.lines[lineID].stations[stationID].yPos = station.yPos;return false;}
+        var overlapX = Math.abs(Math.max(-(Math.min(station.xPos + 10, net.lines[lineID].stations[stationID].xPos + 10) - Math.max(station.xPos, net.lines[lineID].stations[stationID].xPos)), Math.min(station.xPos + 10, net.lines[lineID].stations[stationID].xPos + 10) - Math.max(station.xPos, net.lines[lineID].stations[stationID].xPos)));
+        var overlapY = Math.abs(Math.max(-(Math.min(station.yPos + station.height, net.lines[lineID].stations[stationID].yPos + net.lines[lineID].stations[stationID].height) - Math.max(station.yPos, net.lines[lineID].stations[stationID].yPos)), Math.min(station.yPos + station.height, net.lines[lineID].stations[stationID].yPos + net.lines[lineID].stations[stationID].height) - Math.max(station.yPos, net.lines[lineID].stations[stationID].yPos)));
+        if(overlapX*overlapY > 0 && overlapX*overlapY <= 25 && net.lines.indexOf(line) != lineID){net.lines[lineID].stations[stationID].xPos = station.xPos;net.lines[lineID].stations[stationID].yPos = station.yPos;return false;}
       }
     }
     return true;
