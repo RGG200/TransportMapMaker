b88bc62884e93189d9e24eada09dd683e1f1c4fb
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-09-27 15:14:05 +0200

    Update app.jsLI

--- js/app.js
+++ js/app.js
@@ -222,7 +222,7 @@ export function updateDisplay(event) {
       for(const station of line.stations){
         //var overlapX = Math.max(0, Math.min(station.xPos + 10, net.lines[lineID].stations[stationID].xPos + 10) - Math.max(station.xPos, net.lines[lineID].stations[stationID].xPos));
         //var overlapY = Math.max(0, Math.min(station.yPos + station.height, net.lines[lineID].stations[stationID].yPos + net.lines[lineID].stations[stationID].height) - Math.max(station.yPos, net.lines[lineID].stations[stationID].yPos));
-        if(station.xPos-10 < net.lines[lineID].stations[stationID].xPos && station.xPos+10 > net.lines[lineID].stations[stationID].xPos && station.yPos+station.height/2 > net.lines[lineID].stations[stationID].yPos && station.yPos-station.height/2 < net.lines[lineID].stations[stationID].yPos &&  net.lines.indexOf(line) != lineID){net.lines[lineID].stations[stationID].xPos = station.xPos;net.lines[lineID].stations[stationID].yPos = station.yPos;return false;}
+        if(isUniqueInLine(lineID) && station.xPos > net.lines[lineID].stations[stationID].xPos-10 && station.xPos < net.lines[lineID].stations[stationID].xPos+10 && station.yPos+station.height/2 > net.lines[lineID].stations[stationID].yPos && station.yPos-station.height/2 < net.lines[lineID].stations[stationID].yPos &&  net.lines.indexOf(line) != lineID){net.lines[lineID].stations[stationID].xPos = station.xPos;net.lines[lineID].stations[stationID].yPos = station.yPos;return false;}
       }
     }
     return true;
