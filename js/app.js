f13a762f80b05da6bfc9801ea36c4d6c54042e66
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-10-06 18:25:42 +0200

    Update app.jssvg

--- js/app.js
+++ js/app.js
@@ -230,23 +230,13 @@ export function updateDisplay(event) {
             rectangle2 = stationd;
           }
         })
-        if(intersectRect(rectangle1, rectangle2) && net.lines.indexOf(line) != lineID){
+        if(rectangle1.checkIntersection(rectangle2) && net.lines.indexOf(line) != lineID){
           net.lines[lineID].stations[stationID].xPos = station.xPos;net.lines[lineID].stations[stationID].yPos = station.yPos;return false;
         }
       }
     }
     return true;
   }
-function intersectRect(r1, r2) {
-    var r1 = r1.getBoundingClientRect();    //BOUNDING BOX OF THE FIRST OBJECT
-    var r2 = r2.getBoundingClientRect();    //BOUNDING BOX OF THE SECOND OBJECT
-
-    //CHECK IF THE TWO BOUNDING BOXES OVERLAP
-  return !(r2.left > r1.right || 
-           r2.right < r1.left || 
-           r2.top > r1.bottom ||
-           r2.bottom < r1.top);
-}
 
 
 const canvas = document.getElementById('svg-canvas');
