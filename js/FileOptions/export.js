d9bc541cb2bc921a529aa3757b8219a6dcc07271
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-11-07 12:46:44 +0100

    Update export.js202

--- js/FileOptions/export.js
+++ js/FileOptions/export.js
@@ -6,6 +6,15 @@ function exportSVG(name, width, height){
     });
     return true;
 }
+function downloadSVG(name) {
+  const svg = document.getElementById('container').innerHTML;
+  const blob = new Blob([svg.toString()]);
+  const element = document.createElement("a");
+  element.download = name;
+  element.href = window.URL.createObjectURL(blob);
+  element.click();
+  element.remove();
+}
 function exportPNG(name, width, height){
   document.getElementById('svg-canvas').style.background = '#ffffff';
       svgExport.downloadPng(document.getElementById('svg-canvas'), name, {
