1b569548c3bff88eb4948aecf0d03743e6f91137
RGG200 <75140347+RGG200@users.noreply.github.com>
2023-11-09 21:06:54 +0100

    Add files via upload

--- js/options.js
+++ js/options.js
@@ -1,10 +1,18 @@
-function showOptions(classname){
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+async function showOptions(classname){
+    document.getElementById(classname).style.animation = "fade-in 0.8s";
+    await sleep(600) 
     document.getElementById(classname).style.display = 'block';
 }
-function hideOptions(classname){
-    document.getElementById(classname).style.display = 'none';
+async function hideOptions(classname){
+    document.getElementById(classname).style.animation = "fade-out 0.8s";
+    await sleep(600)
+    document.getElementById(classname).style.display = 'none'
 }
-function showEditor(hidden, showed){
+async function showEditor(hidden, showed){
+    document.getElementById(hidden).style.animation = "fade-out 0.8s";
+    await sleep(800)
     document.getElementById(hidden).style.display = 'none';
     document.getElementById(showed).style.display = 'block';
+    document.getElementById(showed).style.animation = "fade-in 1s";
 }
