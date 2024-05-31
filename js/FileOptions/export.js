c6f1fce247d12dd5a764410398623c95aa82290b
RGG200 <75140347+RGG200@users.noreply.github.com>
2024-05-31 18:14:16 +0200

    kjhi

--- js/FileOptions/export.js
+++ js/FileOptions/export.js
@@ -22,6 +22,3 @@ function exportJPG(){
   document.getElementById('svg-canvas').style.background = 'white';
   svgExport.downloadJpeg(document.getElementById('svg-canvas'), 'transport-map', {width: 2000, height: 2000});
 }
-function resetSVG(){
-  document.getElementById('svg-canvas').style.background = 'url("../img/grid.svg")';
-}
