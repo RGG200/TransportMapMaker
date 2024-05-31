968337f098bc50eb46e25f01759db12e2bb777aa
RGG200 <75140347+RGG200@users.noreply.github.com>
2024-05-31 18:11:26 +0200

    mmma

--- js/FileOptions/export-rtm.js
+++ js/FileOptions/export-rtm.js
@@ -2,7 +2,15 @@
     if(network != null){
         var seen = [];
         var file = new Blob([
-            JSON.stringify(network)], {type: 'application/json'});
+            JSON.stringify(network, function(key, val) {
+               if (val != null && typeof val == "object") {
+                    if (seen.indexOf(val) >= 0) {
+                        return;
+                    }
+                    seen.push(val);
+                }
+                return val;
+            })]);
         const href = URL.createObjectURL(file);
         const a = Object.assign(document.createElement('a'),
         {
