35154e7f1f63d21d7e62e9f664d8cf7fd02a2613
RGG200 <75140347+RGG200@users.noreply.github.com>
2024-11-23 16:47:03 +0100

    Add files via upload

--- js/FileOptions/export.js
+++ js/FileOptions/export.js
@@ -1,20 +1,25 @@
 export function exportSVG(name, wd, hg){
     let wde = parseInt(wd)
     let hge = parseInt(hg)
+    document.getElementById('svg-canvas').style.background = '#ffffff00';
     svgExport.downloadSvg(document.getElementById('svg-canvas'), name, {
         width: wde,
         height: hge,
     });
+    resetCanvas();
 }
 export function exportPNG(name, wd, hg){
+    document.getElementById('svg-canvas').style.background = '#ffffff00';
     let wde = parseInt(wd)*2
     let hge = parseInt(hg)*2
     svgExport.downloadPng(document.getElementById('svg-canvas').outerHTML.toString(), name, {
         width: wde,
         height: hge,
     });
+    document.getElementById('svg-canvas').style.background = 'url("../img/grid.svg")';
 }
 function exportJPG(){
+  document.getElementById('svg-canvas').style.background = 'white';
   svgExport.downloadJpeg(document.getElementById('svg-canvas'), 'transport-map', {width: 2000, height: 2000});
 }
 
